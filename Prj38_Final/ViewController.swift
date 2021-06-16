f0bb053c7132649b2d208a2abe87105c4125f7fb
IGP <pyltsov@gmail.com>
2021-06-16 19:30:54 +0200

    !!++

--- Prj38_Final/ViewController.swift
+++ Prj38_Final/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
+    @IBOutlet weak var secondStepper: UIStepper!
+    @IBOutlet weak var intervalIndicator: UILabel!
+    
+    let center = UNUserNotificationCenter.current()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in }
+        center.delegate = self
     }
 
-
+    @IBAction func secondStepperValueChanged(_ sender: Any) {
+        
+        intervalIndicator.text = String(Int(secondStepper.value))
+        
+    }
+    
+    @IBAction func startBtnPressed(_ sender: UIButton) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "ПРИВЕТ !!!!"
+        content.body = "ТАСС УПОЛНОМОЧЕН ЗАЯВИТЬ..."
+        
+        let interval = Double(intervalIndicator.text ?? "0") ?? 0
+    
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interval, repeats: false)
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { (error) in }
+        
+        
+    }
+    
 }
 
